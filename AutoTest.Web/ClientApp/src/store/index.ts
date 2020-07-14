7c934bb7c673e76702401d9c5276387e4c173eb4
Matt McCorry <mccorry@gmail.com>
2020-07-14 21:47:52 +0100

    fixed date deserialisation

--- AutoTest.Web/ClientApp/src/store/index.ts
+++ AutoTest.Web/ClientApp/src/store/index.ts
@@ -2,13 +2,26 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, createTransform } from "redux-persist";
+import { parseIsoOrThrow } from "ts-date";
 
 import { eventReducer } from "./event/reducers";
 
 const persistConfig = {
     key: "root",
     storage,
+    transforms: [
+        createTransform(JSON.stringify, (toRehydrate) =>
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+            JSON.parse(toRehydrate, (_, value) =>
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                typeof value === "string" &&
+                /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.exec(value)
+                    ? parseIsoOrThrow(value)
+                    : value
+            )
+        ) as never,
+    ],
 };
 
 const rootReducer = combineReducers({
