d36b2f6569a04aa53cf2785aca15ea6fd6db8cfb
Matt McCorry <mccorry@gmail.com>
2020-11-20 08:48:43 +0000

    comment out changes

--- AutoTest.Web/ClientApp/src/api/events.ts
+++ AutoTest.Web/ClientApp/src/api/events.ts
@@ -21,15 +21,20 @@ export const addEvent = async (
     event: Event,
     token: string | undefined
 ): Promise<void> => {
-    const { eventId, ...rest } = event;
-    debugger;
+    const { eventId, regulations, ...rest } = event;
+    // const formData = new FormData();
+    // map(rest, (value, prop) => ({ prop, value })).forEach((a) => {
+    //     formData.append(a.prop, a.value as any);
+    // });
+    // formData.append("regulations", regulations as Blob);
     const response = await fetch(`/api/events/${eventId}`, {
         headers: {
             "Content-Type": "application/json",
             Authorization: token ? `Bearer ${token}` : "",
         },
         method: "PUT",
-        body: JSON.stringify(rest),
+        body: JSON.stringify({ ...rest, regulations: regulations }),
+        // body: formData,
     });
     throwIfNotOk(response);
 };
