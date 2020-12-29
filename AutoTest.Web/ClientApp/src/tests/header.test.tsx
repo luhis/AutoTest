43b68cd8a592e64c0f158924acb6a6350f97e73f
Matt McCorry <mccorry@gmail.com>
2020-12-29 18:31:53 +0000

    fix tests

--- AutoTest.Web/ClientApp/src/tests/header.test.tsx
+++ AutoTest.Web/ClientApp/src/tests/header.test.tsx
@@ -1,10 +1,19 @@
 import { h } from "preact";
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
 import { shallow } from "enzyme";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import Header from "../components/header";
+import { rootReducer } from "../store";
 
 describe("Initial Test of the Header", () => {
+    const store = createStore(rootReducer);
     test("Header renders 3 nav items", () => {
-        shallow(<Header />);
+        shallow(
+            <Provider store={store}>
+                <Header />
+            </Provider>
+        );
     });
 });
