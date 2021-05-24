614fc29ba1bf43773eb76d49e359060d86cb7ea7
Matt McCorry <mccorry@gmail.com>
2021-05-24 07:16:24 +0100

    improve HOC

--- AutoTest.Web/ClientApp/src/components/shared/RouteParamsParser.tsx
+++ AutoTest.Web/ClientApp/src/components/shared/RouteParamsParser.tsx
@@ -1,17 +1,17 @@
-import { createElement, FunctionComponent } from "react";
+/* eslint-disable @typescript-eslint/ban-types */
+import { h, FunctionComponent } from "preact";
 
 import { Override } from "../../types/models";
 
-// eslint-disable-next-line @typescript-eslint/ban-types
 export default <TOuterProps extends object, TInnerProps extends object>(
         paramsMapper: (_: TOuterProps) => TInnerProps
     ) =>
-    (component: FunctionComponent<TInnerProps>) => {
+    (Component: FunctionComponent<TInnerProps>) => {
         const RouteParamsParserWrapper = (
             props: Override<TInnerProps, TOuterProps>
         ) => {
             const finalProps = paramsMapper(props);
-            return createElement(component, finalProps);
+            return <Component {...finalProps} />;
         };
 
         return RouteParamsParserWrapper;
