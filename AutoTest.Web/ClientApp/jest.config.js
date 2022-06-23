aa44c8f10007a00ffbc23b7afdc336f46294bc02
Matt McCorry <mccorry@gmail.com>
2022-06-23 08:32:38 +0100

    update jest config

--- AutoTest.Web/ClientApp/jest.config.js
+++ AutoTest.Web/ClientApp/jest.config.js
@@ -4,7 +4,9 @@ module.exports = {
         "<rootDir>/src/tests/__mocks__/setupTests.js",
         "<rootDir>/src/tests/__mocks__/browserMocks.js"
     ],
-    testURL: "http://localhost:8080",
+    testEnvironmentOptions: {
+        url: "http://localhost:8080",
+    },
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMocks.js",
     },
