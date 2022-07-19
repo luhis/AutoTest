02691d72aff95fe649e4a5bed36610f6bcc126a7
Matt McCorry <mccorry@gmail.com>
2022-07-19 06:38:50 +0100

    add event open/close validation

--- AutoTest.Service/Handlers/SaveEntrantHandler.cs
+++ AutoTest.Service/Handlers/SaveEntrantHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoTest.Domain.Repositories;
 using AutoTest.Domain.StorageModels;
@@ -10,14 +11,26 @@ namespace AutoTest.Service.Handlers
     public class SaveEntrantHandler : IRequestHandler<SaveEntrant, Entrant>
     {
         private readonly IEntrantsRepository entrantsRepository;
+        private readonly IEventsRepository _eventsRepository;
 
-        public SaveEntrantHandler(IEntrantsRepository entrantsRepository)
+        public SaveEntrantHandler(IEntrantsRepository entrantsRepository, IEventsRepository eventsRepository)
         {
             this.entrantsRepository = entrantsRepository;
+            _eventsRepository = eventsRepository;
         }
 
         async Task<Entrant> IRequestHandler<SaveEntrant, Entrant>.Handle(SaveEntrant request, CancellationToken cancellationToken)
         {
+            var @event = await _eventsRepository.GetById(request.Entrant.EventId, cancellationToken);
+            var now = DateTime.UtcNow;
+            if (@event.EntryOpenDate > now)
+            {
+                throw new Exception("Please wait until event open");
+            }
+            if (@event.EntryCloseDate < now)
+            {
+                throw new Exception("Event is now closed");
+            }
             var existing = await entrantsRepository.GetById(request.Entrant.EventId, request.Entrant.EntrantId, cancellationToken);
             request.Entrant.SetPayment(existing?.Payment);
             await entrantsRepository.Upsert(request.Entrant, cancellationToken);
--- Test/AutoTest.Unit.Test/Handlers/SaveEntrantHandlerShould.cs
+++ Test/AutoTest.Unit.Test/Handlers/SaveEntrantHandlerShould.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoTest.Domain.Repositories;
@@ -17,12 +18,14 @@ namespace AutoTest.Unit.Test.Handlers
         private readonly IRequestHandler<SaveEntrant, Entrant> sut;
         private readonly MockRepository mr;
         private readonly Mock<IEntrantsRepository> entrantsRepository;
+        private readonly Mock<IEventsRepository> eventsRepository;
 
         public SaveEntrantHandlerShould()
         {
             mr = new MockRepository(MockBehavior.Strict);
             entrantsRepository = mr.Create<IEntrantsRepository>();
-            sut = new SaveEntrantHandler(entrantsRepository.Object);
+            eventsRepository = mr.Create<IEventsRepository>();
+            sut = new SaveEntrantHandler(entrantsRepository.Object, eventsRepository.Object);
         }
 
         [Fact]
@@ -36,11 +39,49 @@ namespace AutoTest.Unit.Test.Handlers
             var entrantFromDb = new Entrant(entrantId, 123, "name", "familyName", "a@a.com", "A", eventId, "BRMC", 123456, Domain.Enums.Age.Senior);
             entrantsRepository.Setup(a => a.GetById(eventId, entrantId, CancellationToken.None)).ReturnsAsync(entrantFromDb);
             entrantsRepository.Setup(a => a.Upsert(entrant, CancellationToken.None)).Returns(Task.CompletedTask);
+            eventsRepository.Setup(a => a.GetById(eventId, CancellationToken.None)).Returns(Task.FromResult(new Event(eventId, 1, "", DateTime.UtcNow, 3, 2, "", Domain.Enums.EventType.AutoTest, "", Domain.Enums.TimingSystem.StopWatch, DateTime.UtcNow.AddDays(-2), DateTime.UtcNow.AddDays(2))));
+
             var se = new SaveEntrant(entrant);
             var res = await sut.Handle(se, CancellationToken.None);
 
             mr.VerifyAll();
             res.Payment.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task ErrorWhenBeforeEntryStartAsync()
+        {
+            var entrantId = 1ul;
+            var eventId = 2ul;
+            var entrant = new Entrant(entrantId, 123, "name", "familyName", "a@a.com", "A", eventId, "BRMC", 123456, Domain.Enums.Age.Senior);
+            entrant.SetPayment(new Payment());
+
+            eventsRepository.Setup(a => a.GetById(eventId, CancellationToken.None)).Returns(Task.FromResult(new Event(eventId, 1, "", DateTime.UtcNow, 3, 2, "", Domain.Enums.EventType.AutoTest, "", Domain.Enums.TimingSystem.StopWatch, DateTime.UtcNow.AddDays(1), DateTime.UtcNow.AddDays(2))));
+
+            var se = new SaveEntrant(entrant);
+            Func<Task> act = () => sut.Handle(se, CancellationToken.None);
+            var exception = await act.Should().ThrowAsync<Exception>();
+            exception.WithMessage("Please wait until event open");
+
+            mr.VerifyAll();
+        }
+
+        [Fact]
+        public async Task ErrorWhenAfterEntryCloseAsync()
+        {
+            var entrantId = 1ul;
+            var eventId = 2ul;
+            var entrant = new Entrant(entrantId, 123, "name", "familyName", "a@a.com", "A", eventId, "BRMC", 123456, Domain.Enums.Age.Senior);
+            entrant.SetPayment(new Payment());
+
+            eventsRepository.Setup(a => a.GetById(eventId, CancellationToken.None)).Returns(Task.FromResult(new Event(eventId, 1, "", DateTime.UtcNow, 3, 2, "", Domain.Enums.EventType.AutoTest, "", Domain.Enums.TimingSystem.StopWatch, DateTime.UtcNow.AddDays(-2), DateTime.UtcNow.AddDays(-1))));
+
+            var se = new SaveEntrant(entrant);
+            Func<Task> act = () => sut.Handle(se, CancellationToken.None);
+            var exception = await act.Should().ThrowAsync<Exception>();
+            exception.WithMessage("Event is now closed");
+
+            mr.VerifyAll();
         }
 
         [Fact]
@@ -54,6 +95,8 @@ namespace AutoTest.Unit.Test.Handlers
             entrantFromDb.SetPayment(new Payment());
             entrantsRepository.Setup(a => a.GetById(eventId, entrantId, CancellationToken.None)).ReturnsAsync(entrantFromDb);
             entrantsRepository.Setup(a => a.Upsert(entrant, CancellationToken.None)).Returns(Task.CompletedTask);
+            eventsRepository.Setup(a => a.GetById(eventId, CancellationToken.None)).Returns(Task.FromResult(new Event(eventId, 1, "", DateTime.UtcNow, 3, 2, "", Domain.Enums.EventType.AutoTest, "", Domain.Enums.TimingSystem.StopWatch, DateTime.UtcNow.AddDays(-2), DateTime.UtcNow.AddDays(2))));
+
             var se = new SaveEntrant(entrant);
             var res = await sut.Handle(se, CancellationToken.None);
 
