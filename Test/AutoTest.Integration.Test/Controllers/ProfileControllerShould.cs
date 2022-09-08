24eba8bf0d6032205e029dde80e7aed3161cf03d
Matt McCorry <mccorry@gmail.com>
2022-09-08 06:56:20 +0100

    new tests

---
+++ Test/AutoTest.Integration.Test/Controllers/ProfileControllerShould.cs
@@ -0,0 +1,27 @@
+﻿//using System.Net.Http;
+//using System.Threading.Tasks;
+//using AutoTest.Integration.Test.Fixtures;
+//using AutoTest.Web;
+//using FluentAssertions;
+//using Xunit;
+
+//namespace AutoTest.Integration.Test.Controllers
+//{
+//    public class ProfileControllerShould : IClassFixture<CustomWebApplicationFactory<Startup>>
+//    {
+//        private readonly HttpClient unAuthorisedClient;
+
+//        public ProfileControllerShould(CustomWebApplicationFactory<Startup> fixture)
+//        {
+//            this.unAuthorisedClient = fixture.GetUnAuthorisedClient();
+//        }
+
+//        [Fact]
+//        public async Task GetProfile()
+//        {
+//            var res = await unAuthorisedClient.GetAsync("/api/profile");
+//            var content = await res.Content.ReadAsStringAsync();
+//            res.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+//        }
+//    }
+//}
---
+++ Test/AutoTest.Integration.Test/Controllers/TestRunsControllerShould.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using AutoTest.Integration.Test.Fixtures;
+using AutoTest.Web;
+using FluentAssertions;
+using Xunit;
+
+namespace AutoTest.Integration.Test.Controllers
+{
+    public class TestRunsControllerShould : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient unAuthorisedClient;
+
+        public TestRunsControllerShould(CustomWebApplicationFactory<Startup> fixture)
+        {
+            this.unAuthorisedClient = fixture.GetUnAuthorisedClient();
+        }
+
+        [Fact]
+        public async Task GetResults()
+        {
+            var res = await unAuthorisedClient.GetAsync("api/events/1/tests/2/testRuns");
+            var content = await res.Content.ReadAsStringAsync();
+            res.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
