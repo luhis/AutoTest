980c254d039637832ff74077f7693803495f5695
Matt McCorry <mccorry@gmail.com>
2022-09-15 00:49:43 +0100

    improve tests

--- AutoTest.Web/ClientApp/src/tests/agreements.test.ts
+++ AutoTest.Web/ClientApp/src/tests/agreements.test.ts
@@ -5,12 +5,21 @@ describe("Agreements file checker", () => {
         const r = await fetch(MarshalAgreement, {
             method: "HEAD",
         });
-        expect(r.status).toBe(200);
+        expect(r.ok).toBe(true);
     });
     test("Entrant", async () => {
         const r = await fetch(EntrantAgreement, {
             method: "HEAD",
         });
-        expect(r.status).toBe(200);
+        expect(r.ok).toBe(true);
+    });
+    test("Fail", async () => {
+        const r = await fetch(
+            "https://www.motorsportuk.org/wp-content/uploads/2022/02/XXXX2022-03-01-signing-on-declaration-competitor-table.pdf",
+            {
+                method: "HEAD",
+            }
+        );
+        expect(r.ok).toBe(false);
     });
 });
