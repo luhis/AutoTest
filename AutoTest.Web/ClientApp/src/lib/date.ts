40a7896a25cb94a21e6bcfc886506dda4731f6f6
Matt McCorry <mccorry@gmail.com>
2022-10-01 11:37:39 +0900

    fix date timezone bug

--- AutoTest.Web/ClientApp/src/lib/date.ts
+++ AutoTest.Web/ClientApp/src/lib/date.ts
@@ -1,7 +1,12 @@
 import { ValidDate } from "ts-date";
 
 export const getDateTimeString = (d: ValidDate) =>
-    d.toISOString().substring(0, 16);
+    `${d.getFullYear()}-${`${d.getMonth() + 1}`.padStart(2, "0")}-${`${
+        d.getDate() + 1
+    }`.padStart(2, "0")}T${`${d.getHours()}`.padStart(
+        2,
+        "0"
+    )}:${`${d.getMinutes()}`.padStart(2, "0")}`;
 
 export const getDateString = (d: ValidDate | Date) =>
     d.toISOString().substring(0, 10);
