2a3e8a7590b1b18d9272f13e3bd2de8015d2c069
Matt McCorry <mccorry@gmail.com>
2024-01-28 18:48:46 +0000

    tests

---
+++ Test/AutoTest.Unit.Test/Handlers/GetEntrantHandlerShould.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoTest.Domain.Repositories;
+using AutoTest.Domain.StorageModels;
+using AutoTest.Service.Handlers;
+using AutoTest.Service.Messages;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using Xunit;
+
+namespace AutoTest.Unit.Test.Handlers
+{
+    public class GetEntrantHandlerShould
+    {
+        private readonly MockRepository mr;
+        private readonly IRequestHandler<GetEntrant, Entrant?> sut;
+        private readonly Mock<IEntrantsRepository> profileRepository;
+
+        public GetEntrantHandlerShould()
+        {
+            mr = new MockRepository(MockBehavior.Strict);
+            profileRepository = mr.Create<IEntrantsRepository>();
+            sut = new GetEntrantHandler(profileRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetMarshals()
+        {
+            var eventId = 1ul;
+            var entrantId = (ushort)2u;
+            var entrant = new Entrant(1, entrantId, "Joe", "Bloggs", "a@a.com", Domain.Enums.EventType.AutoTest, "A", 99, "BRMC", 123456, Domain.Enums.Age.Senior, false);
+            profileRepository.Setup(a => a.GetById(eventId, entrantId, CancellationToken.None)).ReturnsAsync(entrant);
+
+            var res = await sut.Handle(new(eventId, entrantId), CancellationToken.None);
+
+            res.Should().BeEquivalentTo(entrant);
+            mr.VerifyAll();
+        }
+    }
+}
---
+++ Test/AutoTest.Unit.Test/Handlers/GetMarshalEventsHandlerShould.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoTest.Domain.Repositories;
+using AutoTest.Domain.StorageModels;
+using AutoTest.Service.Handlers;
+using AutoTest.Service.Messages;
+using FluentAssertions;
+using MediatR;
+using MockQueryable.Moq;
+using Moq;
+using Xunit;
+
+namespace AutoTest.Unit.Test.Handlers
+{
+    public class GetMarshalEventsHandlerShould
+    {
+        private readonly MockRepository mr;
+        private readonly IRequestHandler<GetMarshalEvents, IEnumerable<ulong>> sut;
+        private readonly Mock<IMarshalsRepository> profileRepository;
+
+        public GetMarshalEventsHandlerShould()
+        {
+            mr = new MockRepository(MockBehavior.Strict);
+            profileRepository = mr.Create<IMarshalsRepository>();
+            sut = new GetMarshalEventsHandler(profileRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetMarshals()
+        {
+            var eventId = 1ul;
+            var marshals = new[] {
+                new Marshal(1, "b", "b", "a@a.com", eventId, 212312, ""),
+                new Marshal(2, "a", "a", "a@a.com", eventId, 212312, "")
+            };
+            var mock = marshals.BuildMock();
+            profileRepository.Setup(a => a.GetByEmail("test@test.com")).Returns(mock);
+
+            var res = await sut.Handle(new("test@test.com"), CancellationToken.None);
+
+            res.Should().BeEquivalentTo(marshals.OrderBy(a => a.FamilyName).Select(a => a.EventId).Distinct(), o => o.WithStrictOrdering());
+            mr.VerifyAll();
+        }
+    }
+}
