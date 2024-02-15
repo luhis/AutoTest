65430fdfc2dc58982a43bfa16b50c780a222cbd6
Matt McCorry <mccorry@gmail.com>
2024-02-15 23:19:59 +0000

    new test

---
+++ Test/AutoTest.Unit.Test/Mappers/ProfileMapperShould.cs
@@ -0,0 +1,22 @@
+﻿using AutoTest.Domain.StorageModels;
+using AutoTest.Web.Mapping;
+using AutoTest.Web.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace AutoTest.Unit.Test.Mappers
+{
+    public class ProfileMapperShould
+    {
+        [Fact]
+        public void MapSaveModel()
+        {
+            var model = new ProfileSaveModel();
+            var res = MapClub.Map("a@a.com", model);
+
+            var expected = new Profile("a@a.com", "", "", Domain.Enums.Age.Senior, false);
+
+            res.Should().BeEquivalentTo(expected);
+        }
+    }
+}
