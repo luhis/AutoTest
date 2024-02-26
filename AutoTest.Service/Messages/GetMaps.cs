64a2776a355116701789c92806b40272a4f3f96e
Matt McCorry <mccorry@gmail.com>
2024-02-26 19:36:43 +0000

    add file handlers

---
+++ AutoTest.Service/Messages/GetMaps.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace AutoTest.Service.Messages
+{
+    public class GetMaps(ulong eventId) : IRequest<string>
+    {
+        public ulong EventId { get; } = eventId;
+    }
+}
---
+++ AutoTest.Service/Messages/GetRegs.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace AutoTest.Service.Messages
+{
+    public class GetRegs(ulong eventId) : IRequest<string>
+    {
+        public ulong EventId { get; } = eventId;
+    }
+}
