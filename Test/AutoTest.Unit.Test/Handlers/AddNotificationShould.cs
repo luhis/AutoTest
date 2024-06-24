eb49931c5a15045c674eeada11c5e8a33a4aaaf1
Matt McCorry <mccorry@gmail.com>
2024-06-24 22:16:33 +0100

    improve mocking

--- Test/AutoTest.Unit.Test/Handlers/AddNotificationShould.cs
+++ Test/AutoTest.Unit.Test/Handlers/AddNotificationShould.cs
@@ -4,38 +4,29 @@ using AutoTest.Domain.Repositories;
 using AutoTest.Domain.StorageModels;
 using AutoTest.Service.Handlers;
 using AutoTest.Service.Interfaces;
-using AutoTest.Service.Messages;
-using MediatR;
 using Moq;
+using Moq.AutoMock;
 using Xunit;
 
 namespace AutoTest.Unit.Test.Handlers
 {
     public class AddNotificationShould
     {
-        private readonly IRequestHandler<AddNotification> sut;
-        private readonly MockRepository mr;
-        private readonly Mock<IEventNotifier> notifier;
-        private readonly Mock<INotificationsRepository> notificationRepository;
-
-        public AddNotificationShould()
-        {
-            mr = new MockRepository(MockBehavior.Strict);
-            notifier = mr.Create<IEventNotifier>();
-            notificationRepository = mr.Create<INotificationsRepository>();
-            sut = new AddNotificationHandler(notificationRepository.Object, notifier.Object);
-        }
-
         [Fact]
         public async Task NotifyOnNewNotification()
         {
+            var mocker = new AutoMocker(MockBehavior.Strict);
+            var sut = mocker.CreateInstance<AddNotificationHandler>();
+
+            var notifier = mocker.GetMock<IEventNotifier>();
             var notification = new Notification(1, 2, "message", new System.DateTime(2000, 1, 1), "test user");
             notifier.Setup(a => a.NewNotification(notification, CancellationToken.None)).Returns(Task.CompletedTask);
+            var notificationRepository = mocker.GetMock<INotificationsRepository>();
             notificationRepository.Setup(a => a.AddNotification(notification, CancellationToken.None)).Returns(Task.CompletedTask);
 
             await sut.Handle(new(notification), CancellationToken.None);
 
-            mr.VerifyAll();
+            mocker.VerifyAll();
         }
     }
 }
--- Test/AutoTest.Unit.Test/Handlers/AddTestRunShould.cs
+++ Test/AutoTest.Unit.Test/Handlers/AddTestRunShould.cs
@@ -12,6 +12,7 @@ using FluentAssertions;
 using FluentAssertions.ArgumentMatchers.Moq;
 using MediatR;
 using Moq;
+using Moq.AutoMock;
 using OneOf;
 using OneOf.Types;
 using Xunit;
@@ -20,50 +21,41 @@ namespace AutoTest.Unit.Test.Handlers
 {
     public class AddTestRunShould
     {
-        private readonly IRequestHandler<AddTestRun, OneOf<Success, Error<string>>> sut;
-        private readonly MockRepository mr;
-        private readonly Mock<IEventNotifier> notifier;
-        private readonly Mock<ITestRunsRepository> testRuns;
-        private readonly Mock<IEventsRepository> events;
-        private readonly Mock<IMarshalsRepository> marshalsRepository;
         private readonly ICollection<Penalty> penalties = new[] { new Penalty(Domain.Enums.PenaltyEnum.Late, 1) };
-
-        public AddTestRunShould()
-        {
-            mr = new MockRepository(MockBehavior.Strict);
-            notifier = mr.Create<IEventNotifier>();
-            testRuns = mr.Create<ITestRunsRepository>();
-            events = mr.Create<IEventsRepository>();
-            marshalsRepository = mr.Create<IMarshalsRepository>();
-            sut = new AddTestRunHandler(testRuns.Object, notifier.Object, marshalsRepository.Object, events.Object);
-        }
 
         [Fact]
         public async Task ShouldNotifyOnAddedTestRun()
         {
+            var mocker = new AutoMocker(MockBehavior.Strict);
             var entrantId = 5ul;
             var marshalId = 6ul;
             var eventId = 1ul;
             var clubId = 2ul;
             var tr = new TestRun(1, eventId, 3, 4, entrantId, new DateTime(2000, 1, 1), marshalId);
             tr.SetPenalties(penalties);
+            var marshalsRepository = mocker.GetMock<IMarshalsRepository>();
             marshalsRepository.Setup(a => a.GetMarshalIdByEmail(eventId, "marshal@email.com", CancellationToken.None)).ReturnsAsync(marshalId);
             var @event = Models.GetEvent(eventId, clubId);
             @event.SetEventStatus(Domain.Enums.EventStatus.Running);
+            var events = mocker.GetMock<IEventsRepository>();
             events.Setup(a => a.GetById(eventId, CancellationToken.None)).ReturnsAsync(@event);
 
+            var notifier = mocker.GetMock<IEventNotifier>();
             notifier.Setup(a => a.NewTestRun(Its.EquivalentTo(tr), CancellationToken.None)).Returns(Task.CompletedTask);
+            var testRuns = mocker.GetMock<ITestRunsRepository>();
             testRuns.Setup(a => a.AddTestRun(Its.EquivalentTo(tr), CancellationToken.None)).Returns(Task.CompletedTask);
 
+            IRequestHandler<AddTestRun, OneOf<Success, Error<string>>> sut = mocker.CreateInstance<AddTestRunHandler>();
             var res = await sut.Handle(new(1, eventId, 3, 4, entrantId, new DateTime(2000, 1, 1), "marshal@email.com", penalties), CancellationToken.None);
 
             res.AsT0.Should().NotBeNull();
-            mr.VerifyAll();
+            mocker.VerifyAll();
         }
 
         [Fact]
         public async Task ShouldNotAddTestRunWhenNotRunning()
         {
+            var mocker = new AutoMocker(MockBehavior.Strict);
             var entrantId = 5ul;
             var marshalId = 6ul;
             var eventId = 1ul;
@@ -71,12 +63,14 @@ namespace AutoTest.Unit.Test.Handlers
             var tr = new TestRun(1, eventId, 3, 4, entrantId, new DateTime(2000, 1, 1), marshalId);
             tr.SetPenalties(penalties);
             var @event = Models.GetEvent(eventId, clubId);
+            var events = mocker.GetMock<IEventsRepository>();
             events.Setup(a => a.GetById(eventId, CancellationToken.None)).ReturnsAsync(@event);
 
+            IRequestHandler<AddTestRun, OneOf<Success, Error<string>>> sut = mocker.CreateInstance<AddTestRunHandler>();
             var res = await sut.Handle(new(1, eventId, 3, 4, entrantId, new DateTime(2000, 1, 1), "marshal@email.com", penalties), CancellationToken.None);
 
             res.AsT1.Value.Should().Be("Event must be running to add Test Run");
-            mr.VerifyAll();
+            mocker.VerifyAll();
         }
     }
 }
