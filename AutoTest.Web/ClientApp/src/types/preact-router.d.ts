b2b8016bea07d183b2c06e6e743d743b819909e5
Matt McCorry <mccorry@gmail.com>
2026-06-17 21:11:10 +0100

    update preact

---
+++ AutoTest.Web/ClientApp/src/types/preact-router.d.ts
@@ -0,0 +1,14 @@
+import "preact-router";
+import "preact-router/match";
+
+declare module "preact-router" {
+  export function Link(
+    props: import("preact").JSX.AnchorHTMLAttributes<HTMLAnchorElement>,
+  ): import("preact").VNode;
+}
+
+declare module "preact-router/match" {
+  export interface LinkProps {
+    href?: string;
+  }
+}
