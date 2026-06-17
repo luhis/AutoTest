ee8964221ad29b23857510434093b1cc42ce6ac7
Matt McCorry <mccorry@gmail.com>
2026-06-17 21:53:20 +0100

    tidy up configs

--- AutoTest.Web/ClientApp/jest.config.js
+++ AutoTest.Web/ClientApp/jest.config.js
@@ -1,17 +1,19 @@
-module.exports = {
-    preset: "jest-preset-preact",
-    setupFiles: [
-        "<rootDir>/src/tests/__mocks__/setupTests.js",
-        "<rootDir>/src/tests/__mocks__/browserMocks.js"
-    ],
-    testEnvironmentOptions: {
-        url: "http://localhost:8080",
-    },
-    moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mocks__/fileMocks.js",
-    },
-    transformIgnorePatterns: ['../../node_modules/(?!${react-bulma-components})']
-}
+const dotenv = require("dotenv");
 
-const dotenv = require('dotenv');
-dotenv.config({path: './.env.test'});
+dotenv.config({ path: "./.env.test" });
+
+module.exports = {
+  preset: "jest-preset-preact",
+  setupFiles: [
+    "<rootDir>/src/tests/__mocks__/setupTests.js",
+    "<rootDir>/src/tests/__mocks__/browserMocks.js",
+  ],
+  testEnvironmentOptions: {
+    url: "http://localhost:8080",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/tests/__mocks__/fileMocks.js",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!react-bulma-components)/"],
+};
