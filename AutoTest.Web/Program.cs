cd91744029790a485cebe0b7609bfe88ad4222df
Matt McCorry <mccorry@gmail.com>
2026-07-03 21:56:58 +0100

    Seed database on startup in Program.cs
    Updated Program.cs to seed the database before starting the web host. Made Main async, created a service scope, and invoked SeedDatabaseAsync on AutoTestContext. Added necessary using directives for dependency injection and persistence.

--- AutoTest.Web/Program.cs
+++ AutoTest.Web/Program.cs
@@ -1,12 +1,24 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using AutoTest.Persistence;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace AutoTest.Web;
 
 public static class Program
 {
-    public static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        using var scope = host.Services.CreateScope();
+        var autoTestContext = scope.ServiceProvider.GetRequiredService<AutoTestContext>();
+        await autoTestContext.SeedDatabaseAsync();
+
+        await host.RunAsync();
+    }
 
     private static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
