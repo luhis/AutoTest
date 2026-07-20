50b6d8f861bfa581fc404b4fe35d5bc941ce345a
Matt McCorry <mccorry@gmail.com>
2026-07-20 22:57:31 +0100

    Remove unused Mediator using directive in Startup.cs
    The using directive for Mediator was removed from Startup.cs as it was no longer needed. No other changes were made.

--- AutoTest.Web/Startup.cs
+++ AutoTest.Web/Startup.cs
@@ -21,7 +21,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
-using Mediator;
 using WebMarkupMin.AspNetCoreLatest;
 
 namespace AutoTest.Web;
