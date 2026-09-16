dfa13bae55cb51724658af0498cd7db2e862f56b
Matt McCorry <mccorry@gmail.com>
2026-09-16 23:43:18 +0100

    feat(app): implement lazy loading for routes using Suspense

--- ClientApp/src/components/app.tsx
+++ ClientApp/src/components/app.tsx
@@ -1,4 +1,5 @@
 import { type FunctionComponent, h } from "preact";
+import { lazy, Suspense } from "preact/compat";
 import { Route, Router } from "preact-router";
 import { Provider } from "react-redux";
 import { Container, Loader } from "react-bulma-components";
@@ -7,24 +8,25 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 
-import Home from "../routes/home";
-import Profile from "../routes/profile";
-import Club from "../routes/clubs";
-import NotFoundPage from "../routes/notfound";
-import Events from "../routes/events";
-import Event from "../routes/event";
-import Entrant from "../routes/entrants";
-import Marshals from "../routes/marshals";
 import Header from "./header";
-import Results from "../routes/results";
-import Tests from "../routes/tests";
-import Marshal from "../routes/marshal";
-import LiveRuns from "../routes/liveRuns";
-import EditRuns from "../routes/editRuns";
 import store from "../store";
 import { appInsightsKey, googleKey } from "../settings";
 
 import "bulma/css/bulma.min.css";
+
+const Home = lazy(() => import("../routes/home"));
+const Profile = lazy(() => import("../routes/profile"));
+const Club = lazy(() => import("../routes/clubs"));
+const NotFoundPage = lazy(() => import("../routes/notfound"));
+const Events = lazy(() => import("../routes/events"));
+const Event = lazy(() => import("../routes/event"));
+const Entrant = lazy(() => import("../routes/entrants"));
+const Marshals = lazy(() => import("../routes/marshals"));
+const Results = lazy(() => import("../routes/results"));
+const Tests = lazy(() => import("../routes/tests"));
+const Marshal = lazy(() => import("../routes/marshal"));
+const LiveRuns = lazy(() => import("../routes/liveRuns"));
+const EditRuns = lazy(() => import("../routes/editRuns"));
 
 if (import.meta.hot) {
   void import("preact/debug");
@@ -55,24 +57,32 @@ const App: FunctionComponent = () => {
           <GoogleOAuthProvider clientId={googleKey}>
             <Header />
             <Container fluid class="section">
-              <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} />
-                <Route path="/clubs/" component={Club} />
-                <Route path="/events/" component={Events} />
-                <Route path="/event/:eventId" component={Event} />
-                <Route path="/entrants/:eventId" component={Entrant} />
-                <Route path="/marshals/:eventId" component={Marshals} />
-                <Route path="/results/:eventId/:params?" component={Results} />
-                <Route path="/tests/:eventId" component={Tests} />
-                <Route path="/marshal/:eventId/:ordinal" component={Marshal} />
-                <Route
-                  path="/liveRuns/:eventId/:params?"
-                  component={LiveRuns}
-                />
-                <Route path="/editRuns/:eventId/" component={EditRuns} />
-                <NotFoundPage default />
-              </Router>
+              <Suspense fallback={<Loader />}>
+                <Router>
+                  <Route path="/" component={Home} />
+                  <Route path="/profile/" component={Profile} />
+                  <Route path="/clubs/" component={Club} />
+                  <Route path="/events/" component={Events} />
+                  <Route path="/event/:eventId" component={Event} />
+                  <Route path="/entrants/:eventId" component={Entrant} />
+                  <Route path="/marshals/:eventId" component={Marshals} />
+                  <Route
+                    path="/results/:eventId/:params?"
+                    component={Results}
+                  />
+                  <Route path="/tests/:eventId" component={Tests} />
+                  <Route
+                    path="/marshal/:eventId/:ordinal"
+                    component={Marshal}
+                  />
+                  <Route
+                    path="/liveRuns/:eventId/:params?"
+                    component={LiveRuns}
+                  />
+                  <Route path="/editRuns/:eventId/" component={EditRuns} />
+                  <NotFoundPage default />
+                </Router>
+              </Suspense>
             </Container>
           </GoogleOAuthProvider>
         </PersistGate>
